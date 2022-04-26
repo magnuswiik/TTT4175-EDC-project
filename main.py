a084f07755862e0937441443d112756b89346eb8
solnor <solve.norsterud@gmail.com>
2022-04-26 14:51:55 +0200

    langt frem

--- main.py
+++ main.py
@@ -1,4 +1,77 @@
+from email.policy import default
+from nis import match
 from sklearn.datasets import load_iris
+import numpy as np
+import stat_helper as sh
 
 iris = load_iris()
-iris
+
+iris_classes = np.reshape(np.array(iris['target_names']),(1,3))
+iris_data = np.array(iris['data'])
+iris_feature = np.reshape(np.array(iris['feature_names']),(1,4))
+iris_target_predict = np.array(iris['target'])
+iris_target_train = sh.fix_target(np.array(iris['target']),iris_classes)
+
+def iris_train(data,target, start, stop, classes, features, error_margin):
+
+    #Training variables
+    W = np.ones((classes.size,features.size +1)) #+1 for bias 
+    MSE = 10000
+    prevMSE = 0
+    grad_W_MSE = 0
+    alpha = 0.5#make a better alpha
+    count = 0
+    
+    while abs(MSE-prevMSE) > error_margin*0.0000001:
+        count +=1
+        prevMSE = MSE
+        MSE = 0
+        #Dataset acces variables
+        itt = 0
+        skip_len = int(data.shape[0]/classes.size - (stop-start))
+       
+        for i in range (0,classes.size*(stop-start)):
+            if i%(stop-start) == 0 and i!=0:
+                itt+=1
+            index = itt*skip_len + start + i
+            
+            xk = np.reshape(data[index],(data[index].size,1))
+            xk = np.append(xk,1)
+            xk = np.reshape(xk,(xk.size,1))
+            gk = sh.sigmoid(np.matmul(W,xk))
+            tk = np.reshape(target[index],(target[index].size,1))
+
+            MSE += sh.calculate_MSE(gk,tk)
+            grad_W_MSE += sh.calculate_grad_W_MSE(gk,tk,xk) 
+        #update W
+        W -= alpha*grad_W_MSE
+        #print(MSE)
+    print(W)
+    #print(count)
+    return W
+
+def iris_predict(data, W, start, stop, classes, target):
+    #Dataset acces variables
+    itt = 0
+    skip_len = int(data.shape[0]/classes.size - (stop-start))
+
+    confusion_matrix = np.zeros((classes.size,classes.size))
+    for i in range (0,classes.size*(stop-start)):
+
+        if i%(stop-start) == 0 and i!=0:
+            itt+=1
+        index = itt*skip_len + start+ i
+
+        xk = np.reshape(data[index],(data[index].size,1))
+        xk = np.append(xk,1)
+        xk = np.reshape(xk,(xk.size,1))
+
+        gk = np.matmul(W,xk)
+
+        gk, guess = sh.make_percentage_guess(gk)
+        confusion_matrix[target[index]][guess]+=1
+    return confusion_matrix
+
+W = iris_train(iris_data,iris_target_train, 0,30,iris_classes,iris_feature,0.01)
+confusion = iris_predict(iris_data,W,30,50,iris_classes,iris_target_predict)
+print(confusion)
--- stat_helper.py
+++ stat_helper.py
@@ -1,8 +1,20 @@
 import numpy as np
-import pandas as pd
 
-def Sigmoid(x):
-    return np.array(1/(1+ np.exp(-x)))
+def fix_target(target,classes):
+    new_target = np.zeros((1,len(classes)))
+    new_target = np.tile(new_target,(int(len(target)),classes.size))
+
+    print()
+
+    for i in range (len(target)):
+        new_target[i][target[i]] = 1
+    return new_target
+
+def sigmoid(x):
+    for i in range (len(x)):
+        x[i] = 1/(1+ np.exp(-x[i]))
+    x = np.reshape(x,(x.size,1))
+    return x
 
 def grad_gk_mse(gk, tk):
     grad = gk-tk
@@ -14,7 +26,18 @@ def grad_zk_g(gk):
     grad = np.multiply((1-gk),gk)
     return grad
 
-def grad_w_zk(x):
-    x_trans = 
+#def grad_w_zk(x):
+def calculate_MSE(gk,tk):
+    return 0.5*np.matmul((gk-tk).T,(gk-tk))
 
-def w_grad_mse(grad_gk_mse, grad_zk_g, grad_w_zk):
+def calculate_grad_W_MSE(gk, tk, xk):
+    return np.matmul((gk-tk)*gk*(1-gk),xk.T)
+
+def make_percentage_guess(gk):
+    total = np.sum(abs(gk))
+    largest = 0
+    for i in range (0,gk.size):
+        gk[i] = np.around(0.5*(1 - abs(gk[i])/total),2) # gk[i]/np.sum(gk) si how sure it is of not being correct
+        if gk[i] > gk[largest]:
+            largest = i
+    return gk, largest
