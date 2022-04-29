e4f3d3f8ab2697dc74c7fff13d82cc1ce19b53a2
solnor <solve.norsterud@gmail.com>
2022-04-29 14:56:29 +0200

    ferdig med clustering

--- Digits/functions.py
+++ Digits/functions.py
@@ -1,40 +1,76 @@
 import numpy as np
-from math import sqrt
+import matplotlib.pyplot as plt
 from statistics import mode
+from sklearn.cluster import KMeans
 from tensorflow import keras
 from keras.datasets import mnist
+import datetime as dt
+
 
 # Clustering
-def countEachDigit(target):
-    numberOfEach = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
-    for i in range(len(target)):
-        numberOfEach[target[i]] += 1
-    return numberOfEach
+def sortClasses(memory_data, targets):
+    numb_classes = np.zeros(10)
+    sorted_memory_data = np.empty_like(memory_data)
+    sorted_targets = np.argsort(targets)
 
-def euc(img1, img2 ):
+    for i in range(len(targets)):
+        numb_classes[targets[i]] += 1
+        sorted_memory_data[i] = memory_data[sorted_targets[i]]
+
+    return np.asarray(sorted_memory_data), numb_classes
+
+def clustering(memory_data, targets, M):
+    time_start = dt.datetime.now().replace(microsecond=0)
+    
+    sorted_memory_data, numb_classes = sortClasses(memory_data, targets)
+    flattened_sorted_memory_data = sorted_memory_data.flatten().reshape(memory_data.shape[0], 28*28)
+    clusters = np.empty((len(numb_classes), M, 28*28))
+    start = 0
+    end = 0
+
+    for count, i in enumerate(numb_classes):
+        end += i
+        cluster = KMeans(n_clusters=M,random_state=0).fit(flattened_sorted_memory_data[int(start):int(end)]).cluster_centers_
+        start = end
+        clusters[count] = cluster
+
+    time_end = dt.datetime.now().replace(microsecond=0)
+    print("Clustering time: ", time_end-time_start)
+    return clusters.flatten().reshape(len(numb_classes)*64, 28*28)
+
+def eucDist(img1, img2 ):
     img1 = np.reshape(img1, (img1.size,1))
     img2 = np.reshape(img2, (img2.size,1))
     dif = np.array(img1-img2)
     distance = np.matmul(dif.T, dif)
     return distance
 
+def KNNClustering(clusters, test_pic, M):
+    flattened_pic = test_pic.flatten().reshape(1, 28*28)
+    distances = np.zeros(len(clusters))
+
+    for i in range(len(clusters)):
+        distances[i] = np.linalg.norm(flattened_pic - clusters[i])
+    nearest_neighbor_index = np.argmin(distances)
+
+    return nearest_neighbor_index // 64
+
+
 def nearestNeighbors(train, train_targets, test_pic, num_neighbors):
     distances = [[0.]*2 for i in range(train.shape[0])]
     for j in range(train.shape[0]): 
-        #flatImg = flattenImg(train[j])
-        #flatTestImg = flattenImg(test_pic)
-        
         distances[j][0] = train_targets[j]
-        #distances[j][1] = np.linalg.norm(flatImg-flatTestImg)
-        distances[j][1] = euc(train[j], test_pic) #np.linalg.norm(train[j]-test_pic)
+        distances[j][1] = np.linalg.norm(test_pic-train[j]) #euclidian distance
+        #distances[j][1] = eucDist(train[j], test_pic) #np.linalg.norm(train[j]-test_pic)
 
     distances.sort(key=lambda tup: tup[1]) 
-    return distances[0:num_neighbors] 
+    nearest_neighbors = [distances[i][0] for i in range(num_neighbors)]
+    return nearest_neighbors
 
-def KNN(train, train_targets, test_pic, num_neighbors):
-    neighbors = nearestNeighbors(train, train_targets, test_pic, num_neighbors)
-    neighbors_id = [row[0] for row in neighbors]
-    prediction = mode(neighbors_id)
+def KNN(nearest_neighbors):
+    #neighbors = nearestNeighbors(train, train_targets, test_pic, num_neighbors)
+    prediction = mode(nearest_neighbors)
+
     return prediction
 
 def confusionMatrix(predictions, targets):
@@ -54,3 +90,12 @@ def errorRate(confusionMatrix, count):
             if i != j:
                 fails += confusionMatrix[i][j]
     return np.round((fails/count)*100,2)
+
+def plotImg(images, predictions, targets, contenders):
+    for i in range(len(images)):
+        title = "Prediction: " + str(predictions[i]) + " But it was: " + str(targets[i])
+        plt.title(title)
+        plt.xlabel(str(contenders[i]))
+        plt.imshow(images[i])
+        plt.show()
+        input("Press for next pic")
--- Digits/main.py
+++ Digits/main.py
@@ -9,35 +9,70 @@ import datetime as dt
 (train_x, train_y), (test_x, test_y) = mnist.load_data()
 
 # Choose test size
-test_size = 100
+test_size = 1000
 test_collection = test_x[0:test_size]
-targets = test_y[0:test_size]
+test_targets = test_y[0:test_size]
 
 # Choose appropriate memory size
-memory_size = 5000
+memory_size = 60000
 memory_data = train_x[0:memory_size]
-memory_target = train_y[0:memory_size]
+memory_targets = train_y[0:memory_size]
 predictions = np.zeros(test_size)
+
+# Number of neighbors
+num_neighbors = 10
 
 
 def main():
     # Make predictions
     iter = 0
     startTime = dt.datetime.now().replace(microsecond=0)
-    for test_pic in test_collection:
-        prediction = fnc.KNN(memory_data, memory_target, test_pic, 10)
+
+    guesses = np.zeros(test_size)
+
+    clusters = fnc.clustering(memory_data, memory_targets, 64)
+    for i in range(test_size-1):
+        prediction_cluster = fnc.KNNClustering(clusters, test_collection[i], 64)
+        guesses[i] = prediction_cluster
+
+    failures = np.zeros((test_size,len(train_x[0][0]),len(train_x[0][0])))
+    failed_predictions = np.zeros(test_size)
+    failed_targets = np.zeros(test_size)
+    contenders = np.zeros((test_size,num_neighbors))
+
+
+    fail = 0
+    for i in range(test_size-1):  
+        test_pic = test_collection[i]
+        test_target = test_targets[i]
+        neighbors = fnc.nearestNeighbors(memory_data, memory_targets, test_pic, num_neighbors)
+        prediction = fnc.KNN(neighbors)
         predictions[iter] += prediction
+        if prediction != test_target:
+            failures[fail] = test_pic
+            failed_predictions[fail] = prediction
+            failed_targets[fail] = test_target
+            contenders[fail] = neighbors
+            fail += 1
+            print("fuck ",fail)
         iter += 1
 
+
+
     # Make confusion matrix and calculate error rate
-    confusionMatrix = fnc.confusionMatrix(predictions, targets)
+    confusionMatrix = fnc.confusionMatrix(predictions, test_targets)
+    errorRate = fnc.errorRate(confusionMatrix, test_size)
+
+    confusionMatrix = fnc.confusionMatrix(predictions, test_targets)
     errorRate = fnc.errorRate(confusionMatrix, test_size)
 
     endTime = dt.datetime.now().replace(microsecond=0)
     # Print results
     print("Confusion matrix: \n", confusionMatrix)
     print("Error rate: ", errorRate)
-    print("Time used ",endTime-startTime)
+    print("Classification time: ", endTime-startTime)
+
+    fnc.plotImg(failures, failed_predictions, failed_targets, contenders)
 
 main()
 
