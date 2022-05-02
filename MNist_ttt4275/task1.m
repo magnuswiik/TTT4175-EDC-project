bc418e9c5c6d9df66c500a5b4849210d7ca3c27c
erlwit <97439170+erlwit@users.noreply.github.com>
2022-05-02 22:07:20 +0200

    fixed everything bare lever

--- MNist_ttt4275/task1.m
+++ MNist_ttt4275/task1.m
@@ -1,8 +1,34 @@
 load("data_all.mat")
 
 data_all = load("data_all.mat");
+startTime = clock;
 dataChuncks = chunckData(data_all,10);
-preds = KNN(dataChuncks(1),7);
+predsKort = KNN(dataChuncks(1),7);
+confMatKort = calculateConfusionMatrix(predsKort,dataChuncks(1).testlab);
+errorRateKort = calculateErrorRate(confMatKort);
+
+%preds = KNN(data_all,7);
+%confMat = calculateConfusionMatrix(preds,data_all.testlab);
+%errorRate = calculateErrorRate(confMat);
+
+endTime = clock;
+timeTaken = endTime-startTime;
+
+function errorRate = calculateErrorRate(confMat)
+    n_preds = sum(confMat,'all');
+    correct = 0;
+    for i = 1:length(confMat)
+        correct = correct + confMat(i,i);
+    end
+    errorRate = round(100*(1-correct/n_preds),2);
+end
+
+function confusionMatrix = calculateConfusionMatrix(predictions,targets)
+    confusionMatrix = zeros(10,10);
+    for i = 1:length(predictions)
+        confusionMatrix(predictions(i)+1,targets(i)+1) = confusionMatrix(predictions(i)+1,targets(i)+1) + 1;
+    end
+end
 
 function dataChuncks = chunckData(data_all,n_chuncks)
     for i = 1:n_chuncks
